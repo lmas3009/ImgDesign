8b8534d9133eb328b0a19fd7ae07689466fdeeb9
Lmas Lmas <30lmas09@gmail.com>
2020-03-26 18:59:35 +0530

    img

---
+++ ImgDesign.js
@@ -0,0 +1,15 @@
+function ImgDesign(options){
+    let images = document.querySelectorAll(".ImgDesign");
+
+    if(options.type == "hard")
+        options.type = "0px"
+    else    
+        options.type = "15px"
+    
+
+        images.forEach(img=>{
+            img.style.color="red";
+        })
+}
+
+module.exports.ImgDesign = ImgDesign;
