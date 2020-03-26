1630b30d92b0aed8658017224af79fe92244edb6
Lmas Lmas <30lmas09@gmail.com>
2020-03-26 20:59:12 +0530

    edit

--- ImgDesign.js
+++ ImgDesign.js
@@ -1,15 +1,16 @@
 function ImgDesign(options){
     let images = document.querySelectorAll(".ImgDesign");
-
-   
         images.forEach(img=>{
             img.style.color='red';
         })
-
-    let info = document.querySelectorAll(".hdes");
-    info.forEach(hdes=>{
+    let hdes = document.querySelectorAll("h1");
         hdes.style.color='red';
-    })
+
+    if(options.heading_type === "hard"){
+        hdes.style.fontSize = "500px";
+    }
+
+
 }
 
 module.exports.ImgDesign = ImgDesign;
